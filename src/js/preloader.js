7dcca44e895c5f083ccf01bb6207d4665dbee04f
Oleksandr Kovshar <aleksandr.mpro@gmail.com>
2023-09-29 21:30:09 +0300

    v55

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,21 +1,21 @@
-import { refs } from './refs';
-import 'animate.css';
+import { refs } from "./refs";
+import "animate.css";
 
 const img = new Image();
 img.src =
   // 'https://media.tenor.com/ycXJZJnD2SwAAAAd/welcome-welcome-message-for-the-group.gif';
-  'https://media.tenor.com/Yvzvgu1EsA4AAAAd/%D0%BB%D0%B8%D1%81-metro-goldwyn-mayer.gif'
+  "https://media.tenor.com/Yvzvgu1EsA4AAAAd/%D0%BB%D0%B8%D1%81-metro-goldwyn-mayer.gif";
 let i = 0;
 
-window.addEventListener('beforeunload', () => {
+window.addEventListener("beforeunload", () => {
   if (i === 0) {
-    localStorage.removeItem('preloaderPlayed');
+    localStorage.removeItem("preloaderPlayed");
   } else {
     return;
   }
 });
 
-const linkOnSecondPageLogo = document.querySelector('.nav__logo');
+const linkOnSecondPageLogo = document.querySelector(".nav__logo");
 
 let linkOnSecondPageHome;
 
@@ -47,37 +47,37 @@ if (
 }
 
 let search;
-let searchTest = document.querySelectorAll('.search-form');
+let searchTest = document.querySelectorAll(".search-form");
 if (searchTest.length > 0) {
-  search = document.querySelector('.search-form');
-  search.addEventListener('submit', onSecondPage);
+  search = document.querySelector(".search-form");
+  search.addEventListener("submit", onSecondPage);
 }
 
-linkOnSecondPageLogo.addEventListener('click', onSecondPage);
-linkOnSecondPageHome.addEventListener('click', onSecondPage);
-linkOnSecondPageLibrary.addEventListener('click', onSecondPage);
+linkOnSecondPageLogo.addEventListener("click", onSecondPage);
+linkOnSecondPageHome.addEventListener("click", onSecondPage);
+linkOnSecondPageLibrary.addEventListener("click", onSecondPage);
 
-if (!localStorage.getItem('preloaderPlayed')) {
-  window.addEventListener('load', welcome);
-  localStorage.setItem('preloaderPlayed', true);
+if (!localStorage.getItem("preloaderPlayed")) {
+  window.addEventListener("load", welcome);
+  localStorage.setItem("preloaderPlayed", true);
 } else {
-  refs.preloaderGifWrapper.style.display = 'none';
+  refs.preloaderGifWrapper.style.display = "none";
 }
 
 function onSecondPage() {
-  localStorage.setItem('preloaderPlayed', true);
+  localStorage.setItem("preloaderPlayed", true);
   i += 1;
 }
 
 function welcome() {
-  refs.preloaderGifImage.setAttribute('src', img.src);
-  refs.preloaderGifImage.classList.add('animate__fadeIn');
+  refs.preloaderGifImage.setAttribute("src", img.src);
+  refs.preloaderGifImage.classList.add("animate__fadeIn");
 
   setTimeout(() => {
-    refs.preloaderGifWrapper.classList.add('animate__fadeOut');
-    refs.preloaderGifImage.classList.add('animate__fadeOut');
+    refs.preloaderGifWrapper.classList.add("animate__fadeOut");
+    refs.preloaderGifImage.classList.add("animate__fadeOut");
     setTimeout(() => {
-      refs.preloaderGifWrapper.style.display = 'none';
+      refs.preloaderGifWrapper.style.display = "none";
     }, 500);
   }, 2000);
 }
